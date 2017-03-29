f8c1a0c15c70183e4759d6d0aa31a5cc50aa1c42
shiraz Lavi <shirazlavi1@gmail.com>
2017-03-29 08:54:28 +0000

    now it is under post-receive

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello{
 
 public static void main(String[] args){
 
-System.out.println("Hi It is under post-push with +x");
+System.out.println("Hi It is under post-recieve with +x");
  
 	}
 }
